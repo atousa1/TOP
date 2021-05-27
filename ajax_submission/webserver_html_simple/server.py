08acf3574aff0b5cbfcf024b9737d04f9fd93d35
Mehran Hamedani Nezhad <mehran.hamedaninezhad@gmail.com>
2021-05-27 12:26:29 +0430

    second review

---
+++ ajax_submission/webserver_html_simple/server.py
@@ -0,0 +1,28 @@
+import http.server
+import socketserver
+import re
+import os
+
+class AtousaHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+
+        self.send_response(200)
+
+        self.send_header("Content-type", "text/html")
+        
+        self.end_headers()
+
+        htmlFilePath = os.getcwd() + "/ajax_submission/webserver_html_simple/index.html"
+
+        with open(htmlFilePath, 'rb') as fin:
+            self.copyfile(fin, self.wfile)
+
+        return
+
+handler = AtousaHttpRequestHandler
+
+PORT = 9091
+my_server = socketserver.TCPServer(("", PORT), handler)
+
+my_server.serve_forever()
